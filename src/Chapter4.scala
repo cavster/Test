a7d9ad34fdb32e3dadb8460ea093da16e9ebb557
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2014-05-05 00:23:22 +0100

    changes

--- src/Chapter4.scala
+++ src/Chapter4.scala
@@ -1,6 +1,8 @@
 /**
  * Created by colmcavanagh on 5/3/14.
  */
-object Chapter4 {
-
+object Chapter4 extends App{
+  val items:Map[String,Double]=Map("phone"->100.5,"Iphone"->4000,"Colm"->6045)//you need to declare types remember!
+  val newItems= (for((k,v)<- items)yield(k,v*0.9))
+  println(newItems)
 }
